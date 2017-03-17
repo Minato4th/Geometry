ddc235d5317520308554d318b6952f1e98bd3339
Minato4th <borisko.aleksey@gmail.com>
2017-03-17 18:02:59 +0200

    Add files via upload

---
+++ src/Circle.java
@@ -0,0 +1,19 @@
+
+public class Circle extends Figure {
+
+    public Circle(int side1) {
+        super(side1);
+    }
+
+    public double perimeterCircle(){
+        return getSide1() * 2 * 3.14;
+    }
+
+    public double areaCircle(){
+        return getSide2() * getSide1() * 3.14;
+    }
+
+    public double angleCircle(){
+        return 360;
+    }
+}
